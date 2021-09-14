6c3d81ffe1bf5a2c773f9d9f0cf78fd5e1961780
Adam Dubey <adubey0311@gmail.com>
2021-09-14 11:50:19 -0400

    [AD][09M1421] Adding S3 Bucket stack

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from stacks.vpc_stack import VPCStack
 from stacks.security_stack import SecurityStack
 from stacks.bastion_stack import BastionStack
 from stacks.kms_stack import KMSStack
+from stacks.s3_stack import S3Stack
 
 app = core.App()
 
@@ -12,5 +13,6 @@ vpc_stack = VPCStack(app, 'vpc')
 security_stack = SecurityStack(app, 'security-stack', vpc = vpc_stack.vpc)
 bastion_stack = BastionStack(app, 'bastion', vpc = vpc_stack.vpc, sg = security_stack.bastion_sg)
 kms_stack = KMSStack(app, 'kms')
+s3_stack = S3Stack(app, 's3buckets')
 
 app.synth()
