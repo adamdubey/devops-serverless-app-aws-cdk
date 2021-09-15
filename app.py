5c4337731b111cea7b676003eab89e6b3e2aef42
Adam Dubey <adubey0311@gmail.com>
2021-09-15 10:52:53 -0400

    [AD][09M1521] Adding Cognito Stack

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from stacks.kms_stack import KMSStack
 from stacks.s3_stack import S3Stack
 from stacks.rds_stack import RDSStack
 from stacks.redis_stack import RedisStack
+from stacks.cognito_stack import CognitoStack
 
 app = core.App()
 
@@ -18,5 +19,6 @@ kms_stack = KMSStack(app, 'kms')
 s3_stack = S3Stack(app, 's3buckets')
 rds_stack = RDSStack(app, 'rds', vpc = vpc_stack.vpc, lambdasg = security_stack.lambda_sg, bastionsg = security_stack.bastion_sg, kmskey = kms_stack.kms_rds)
 redis_stack = RedisStack(app, 'redis', vpc = vpc_stack.vpc, redissg = core.Fn.import_value('redis-sg-export'))
+cognito_stack = CognitoStack(app, 'cognito')
 
 app.synth()
