5694ff83f280a208ee806e903926d357e6afbb8e
Adam Dubey <adubey0311@gmail.com>
2021-09-15 11:30:50 -0400

    [AD][09M1521] Adding API GW Stack

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from stacks.s3_stack import S3Stack
 from stacks.rds_stack import RDSStack
 from stacks.redis_stack import RedisStack
 from stacks.cognito_stack import CognitoStack
+from stacks.apigw_stack import APIStack
 
 app = core.App()
 
@@ -20,5 +21,6 @@ s3_stack = S3Stack(app, 's3buckets')
 rds_stack = RDSStack(app, 'rds', vpc = vpc_stack.vpc, lambdasg = security_stack.lambda_sg, bastionsg = security_stack.bastion_sg, kmskey = kms_stack.kms_rds)
 redis_stack = RedisStack(app, 'redis', vpc = vpc_stack.vpc, redissg = core.Fn.import_value('redis-sg-export'))
 cognito_stack = CognitoStack(app, 'cognito')
+apigw_stack = APIStack(app, 'apigw')
 
 app.synth()
